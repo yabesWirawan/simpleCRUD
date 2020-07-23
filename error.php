354d4aa18d34c1345a68e7030d6608b35b941181
Natanael Yabes <yabes.wirawan@gmail.com>
2020-07-24 03:52:07 +0900

    Update error.php

--- error.php
+++ error.php
@@ -6,10 +6,10 @@
     <link rel="stylesheet" href="Assets/css/master.css" media="screen" title="no title" charset="utf-8">
 </head>
 <body>
-    <center><h1>Whoops!!!</h1></center>
+    <center><h1>Error</h1></center>
     <center>
         <p>
-            Tidak ada apapun disini ...
+            Halaman ini tidak ditemukan ...
         </p>
     </center>
     <center><a href="index.php" class="button gray">Kembali ke halaman utama</a></center>
