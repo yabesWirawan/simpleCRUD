bae5b58d96583f25159d9ad7c05d30f4fc6d4ecc
Natanael Yabes <yabes.wirawan@gmail.com>
2020-07-24 04:56:11 +0900

    Update insert_mahasiswa.php

--- Query/insert_mahasiswa.php
+++ Query/insert_mahasiswa.php
@@ -5,13 +5,13 @@ require_once(dirname(__DIR__)."/Models/Settings/Conn.php");
 // Require all modules
 require_once(dirname(__DIR__)."/Models/Database/Query.php");
 
-// Ambil semua variable dari form pada halaman add.php
+// Ambil semua variable dari form pada halaman add.php.
 // Cocokan variable dengan nama input tagnya.
 // Polanya adalah: $nama_var = $_POST['nama_tag_input']
 
-// Contoh: untuk tag input nama
+// Contoh: untuk tag input nama,
 // <input type="text" name="nama" autofocus="autofocus"><br><br>
-// name-nya adalah nama, maka
+// name-nya adalah nama, maka:
 // $nama = $_POST['nama']
 
 $nrp = $_POST['nrp'];
@@ -19,7 +19,7 @@ $nama = $_POST['nama'];
 $fakultas = $_POST['fakultas'];
 $jurusan = $_POST['jurusan'];
 
-// Query INSERT mahasiswa ke dalam database
+// Query INSERT mahasiswa ke dalam database.
 $mahasiswa = new Query($conn, "
 INSERT INTO mahasiswa VALUES(NULL,'{$nama}','{$fakultas}','{$jurusan}');
 ");
@@ -27,6 +27,6 @@ INSERT INTO mahasiswa VALUES(NULL,'{$nama}','{$fakultas}','{$jurusan}');
 // Execute query
 $mahasiswa->exec();
 
-// Kembali ke halaman add.php, bisa juga diganti ke halaman index.php ^_^
+// Kembali ke halaman add.php.
 header('Location: ../add.php');
 exit();
